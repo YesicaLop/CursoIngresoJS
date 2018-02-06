9024ce73cdd8c28ebed2c92d40cf603d82deac0a
Yesicalop <yesica_lopez88@hotmail.com>
2018-02-06 11:33:20 -0300

    parcial1

--- 9-Parciales/parcial/uno.js
+++ 9-Parciales/parcial/uno.js
@@ -1,5 +1,11 @@
 
 function Mostrar()
 {
-
-}
+  var lado;
+  var perimetro;
+  lado=document.getElementById("laBase").value;
+  lado=parseInt(lado);
+  perimetro=parseInt(perimetro); 
+  perimetro=lado*4;
+  alert("el perimetro es: " +perimetro);
+}  
