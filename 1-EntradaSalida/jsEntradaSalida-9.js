81ccdb3405a5922bd792f6ee825aef00f90e92e7
Yesicalop <yesica_lopez88@hotmail.com>
2018-02-06 11:09:41 -0300

    e-s,ejercicio9

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,18 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {
+ var nro1;
+    var nro2;
+    var porc;
+    var rta;
 
+    nro1=document.getElementById("sueldo").value;
+    nro2=document.getElementById("resultado").value;
+    nro1=parseInt(nro1);
+    nro2=parseInt(nro2); 
+    porc=(nro1*10)/100;
+    rta=nro1+porc;
+    document.getElementById("resultado").value=rta;
+    alert("el aumento del 10% es: "+porc);
 	
 }
