60d1bcb41e9cef8922201ce9c4ce9d1f5ecc1f1a
Yesicalop <yesica_lopez88@hotmail.com>
2018-02-06 11:16:49 -0300

    e-s,ejercicio10

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,19 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarAumento()
 {
+	var nro1;
+    var nro2;
+    var porc;
+    var rta;
+
+    nro1=document.getElementById("importe").value;
+    nro2=document.getElementById("resultado").value;
+    nro1=parseInt(nro1);
+    nro2=parseInt(nro2); 
+    porc=(nro1*25)/100;
+    rta=nro1-porc;
+    nro2=document.getElementById("resultado").value=rta;
+    alert("el descuento del 25% es: "+porc);
 	
 }
+
