2e0cf2af895e0b4de20a37b5c587c7ff1b4d5ea8
Yesicalop <yesica_lopez88@hotmail.com>
2018-02-06 11:45:55 -0300

    parcial2

--- 9-Parciales/parcial/dos.js
+++ 9-Parciales/parcial/dos.js
@@ -1,4 +1,12 @@
 function Mostrar()
 {
-  
+  var importe;
+  var iva;
+  var total;
+   importe=prompt("Ingrese el Importe");
+   
+   importe=parseInt(importe);
+   iva=(importe*25)/100;
+   total=iva+importe;
+   document.getElementById("importeFinal").value=total;
 }
